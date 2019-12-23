0f3dab7990afddc34d8f1ed465cfebadb02ea088
lazzyang <lazzyang@qq.com>
2019-12-23 20:28:00 +0800

    git commit demo.py

--- AUTOEXCEL/demo.py
+++ AUTOEXCEL/demo.py
@@ -5,10 +5,15 @@ wb = load_workbook(r"C:\Users\liuyang\Desktop\import_template\运单\运单.xlsx
 # wb_q = load_workbook('./清单.xlsx')
 sheets = wb.sheetnames # 得到所有的sheet
 print(sheets)
-sheet = wb[sheets[0]] # 取第一个sheet
-print(sheet)
+ws = wb[sheets[0]] # 取第一个sheet的名字设为ws
+print(ws)
+
+# 打印第二行的所有数据
 
 # 订单的（电商企业代码、电商企业名称）= 清单（申报企业代码、申报企业名称）
+# 定义电商企业代码为ebp_code、名称为ebp_name
+
+
 
 # 运单的（物流企业代码、物流企业名称）= 清单（物流企业代码、物流企业名称）
 
